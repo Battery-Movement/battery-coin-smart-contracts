f8d9e2ba890a4bf72d0071d45b3e6dac81f013eb
Harris Warren <harris.warren@gmail.com>
2025-07-20 21:14:50 -0700

    feat: Isolate smart contracts into dedicated repository

--- src/assets/data/bannerV3.js
+++
@@ -1,9 +0,0 @@
-const Data = {
-  presaleStatus: "Presale Start in",
-  title: "Presale",
-  title2: "Battery Coin",
-  subtitle:
-    "Buy tokens now and reap the benefits of the blockchain revolution!",
-};
-
-export default Data;
